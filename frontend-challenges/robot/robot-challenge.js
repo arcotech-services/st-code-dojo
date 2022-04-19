ec9bf1e234911851daabc73b15be4cc569e2be5a
raiane-honorato <raianehonorato16@gmail.com>
2022-04-19 16:14:51 -0300

    feat(robot): finish challenge

--- frontend-challenges/robot/robot-challenge.js
+++ frontend-challenges/robot/robot-challenge.js
@@ -1,37 +1,62 @@
-function robot() {
-	const orientations = ['N', 'E', 'S', 'W']
+function robot(instruction) {
+  const instructionArray = typeof instruction == 'string' ? instruction.split('') : [];
+	const orientations = ['N', 'E', 'S', 'W']; //indice de qual direção ele está [0, 1, 2, 3]
 	const initialOrientation = [7, 3];
-  const coordinates = { x: 0, y: 0 };
 
   let currentDirection = 0;
-  const instruction = "RAALAL" 
 
+  const turnRight = () => {
+    if(currentDirection === orientations.length - 1) {
+      currentDirection = 0;
+      return;
+    }
+    
+    currentDirection++;
+  }
 
-  const changeDirection = (instruction) => {
-    if(instruction === 'R') {
-        if(currentDirection >= orientations.length){
-					currentDirection = 0
-        }
-        currentDirection++
-    } else {
-        if(currentDirection === 0){
-            currentDirection = 3
-        }
-        currentDirection--
+  const turnLeft = () => {
+    if(currentDirection === 0){
+      currentDirection = orientations.length - 1;
+      return;
+    }
+
+    currentDirection--;
+  }
+
+  const advanced = () => {
+    switch (currentDirection){
+      case 0:
+        initialOrientation[1]++;
+        break;
+      case 1:
+        initialOrientation[0]++;
+        break;
+      case 2:
+        initialOrientation[1]--;
+        break;
+      case 3:
+        initialOrientation[0]--;
+        break;
     }
   }
-}
 
-function movement(action) {
-  let direction;
-  let coordinates;
-
-  switch(action) {
-    case 'L':
-      directions
+  const changeDirection = (instruction) => {
+    switch (instruction) {
+      case 'R':
+        turnRight();
+        break;
+      case 'L':
+        turnLeft();
+        break;
+      case 'A':
+        advanced();
+        break;
+    }
   }
+
+  instructionArray.map(item => changeDirection(item));
+  console.log('direction:::', initialOrientation)
+
 }
 
-  N
-W   E
-  S
+robot('A')
